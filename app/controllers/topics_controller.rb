44456d71018c7f1ffe1eb4a3d10703a898455b4f
mehgellan <baumegg@gmail.com>
2016-09-10 15:15:12 -0700

    user can create a topic and see it on their profile

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,17 +1,46 @@
 class TopicsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:index, :new, :create, :show]
+  # before_action :require_ownership!, only: [:edit, :update, :destroy]
 
   def index
     @topics = Topic.all
   end
 
   def new
+    @topic = Topic.new
   end
 
   def create
+    @topic = current_user.topics.new(topic_params)
+    @user.topics << @topic
+    if @topic.save
+      flash[:success] = "Your topic, #{@topic.name} was successfully created!"
+      redirect_to user_path(@user)
+    else
+      flash[:error] = @topic.errors.full_messages.join(", ")
+      redirect_to new_user_topic_path(@user)
+    end
   end
 
   def show
   end
+
+  private
+
+    def set_user
+      @user = User.find_by_id(params[:user_id])
+    end
+
+    def topic_params
+      params.require(:topic).permit(:name)
+    end
+
+    # def require_ownership!
+    #   if !current_user_is_owner?
+    #     flash[:error] = "You must be logged in as #{@topic.user.first_name}"
+    #     redirect_to login_path
+    #   end
+    # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'users#new'
+  root to: 'users#show'
   get '/signin' => 'users#new', as: 'new_user'
   post '/users' => 'users#create'
   get '/users/:id' => 'users#show', as: 'user'
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   post '/sessions' => 'sessions#create'
 
   get '/users/:user_id/topics' => 'topics#index', as: 'user_topics'
+  get '/users/:user_id/topics/new' => 'topics#new', as: 'new_user_topic'
+  post '/users/:user_id/topics' => 'topics#create'
 
 end
