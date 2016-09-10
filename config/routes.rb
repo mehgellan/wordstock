31b445d56d90a27ee3af70b5088217c969e9ebec
mehgellan <baumegg@gmail.com>
2016-09-09 17:25:10 -0700

    user can log in and be redirected to profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'users#new'
-  get '/users/new' => 'users#new', as: 'new_user'
+  get '/signin' => 'users#new', as: 'new_user'
   post '/users' => 'users#create'
   get '/users/:id' => 'users#show', as: 'user'
+
+  get "/login" => 'sessions#new', as: 'login'
+  post "/sessions" => "sessions#create"
 end
